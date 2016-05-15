3817071107a9ffbc0f0cae42be629b5714e52c82
nohman20 <nohman20@gmail.com>
2016-05-15 12:45:24 -0700

    views and moved login.js

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
-var express = require('Express')
+var express = require('Express'),
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  app = new express()
 
-var app = new express()
+app.use(bodyParser.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 app.get('/', function (req, res) {
-  res.render('')
+  res.render('layout', {data: peoples})
+// es.json {peoples: JSON.stringify(peoples), times: JSON.stringify(times)})
 })
+
+app.get('/login', function (req, res) {
+  res.render('loginForm')
+})
+
+app.get('/signup', function (req, res) {
+  res.render('signupForm')
+})
+
+var times = [2000, 300, 400, 600, 1200]
+var peoples = [{
+  'name': 'Aaron',
+  'justification': 'I lika da poop'
+}, {
+  'name': 'T-RAD',
+  'justification': 'How you like me now bitch'
+}, {
+  'name': 'Pepino',
+  'justification': 'I am Spartacus'
+}, {
+  'name': 'Turd Burglar',
+  'justification': "That's not what your mother said last night Trebek"
+}, {
+  'name': 'Teenage Mutant Ninja',
+  'justification': 'Turdles'
+}, {
+  'name': 'Donald Dump',
+  'justification': "I POOP, I have da best poops, they're UUGE"
+}]
+app.listen(3000)
+
+/* app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'))
+  //__dirname : It will resolve to your project folder.
+}); */
