f39cab262a32d47d6545eb181e4e53ff9a880380
Roger Filmyer <roger.filmyer@gmail.com>
2016-05-14 18:42:48 -0700

    testing out the reader

--- js/test.js
+++ js/test.js
@@ -1,7 +1,16 @@
 var detection = require('./detection.js');
+var mraa = require('mraa');
 var noisebridge = new detection.Bathroom();
 noisebridge.isOn(true);
 console.log(noisebridge.currentSession);
 noisebridge.isOn(false);
 console.log(noisebridge.currentSession);
 setTimeout(function () { console.log('Past Sessions: ' + noisebridge.pastSessions); }, noisebridge.minTimeGap + 500);
+var switchGPIO = new mraa.Gpio(6);
+switchGPIO.dir(mraa.DIR_IN);
+function pollSwitch() {
+    var status = switchGPIO.read();
+    noisebridge.isOn(status == true);
+    console.log(noisebridge.currentSession);
+    setTimeout(pollSwitch, 1000);
+}
--- ts/test.ts
+++ ts/test.ts
@@ -1,5 +1,6 @@
 declare var require: any;
 var detection = require('./detection.js');
+var mraa = require('mraa')
 
 var noisebridge = new detection.Bathroom();
 noisebridge.isOn(true);
@@ -7,3 +8,13 @@ console.log(noisebridge.currentSession)
 noisebridge.isOn(false);
 console.log(noisebridge.currentSession)
 setTimeout(function(){console.log('Past Sessions: ' + noisebridge.pastSessions)}, noisebridge.minTimeGap + 500);
+
+var switchGPIO = new mraa.Gpio(6);
+switchGPIO.dir(mraa.DIR_IN);
+
+function pollSwitch(){
+	var status = switchGPIO.read();
+	noisebridge.isOn(status == true);
+	console.log(noisebridge.currentSession);
+	setTimeout(pollSwitch, 1000);
+}
