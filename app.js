caa3ae5b3d98a1ba391372c13bc2c0cfbaa7d532
nohman20 <nohman20@gmail.com>
2016-05-15 15:26:19 -0700

    mockup

--- app.js
+++ app.js
@@ -5,45 +5,19 @@ var express = require('Express'),
 
 app.use(bodyParser.json())
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
 
 app.get('/', function (req, res) {
-  res.render('rankingForm', {data: peoples})
-// es.json {peoples: JSON.stringify(peoples), times: JSON.stringify(times)})
+  res.sendFile('layout.html')
 })
 
 app.get('/login', function (req, res) {
-  res.render('loginForm')
+  res.sendFile('loginForm.html')
 })
 
 app.get('/justify', function (req, res) {
-  res.render('shiTestimony')
+  res.sendFile('shiTestimony.html')
 })
 
-app.get('/signup', function (req, res) {
-  res.render('signupForm')
-})
-
-var times = [2000, 300, 400, 600, 1200]
-var peoples = [{
-  'name': 'Aaron',
-  'justification': 'I lika da poop'
-}, {
-  'name': 'T-RAD',
-  'justification': 'How you like me now bitch'
-}, {
-  'name': 'Pepino',
-  'justification': 'I am Spartacus'
-}, {
-  'name': 'Turd Burglar',
-  'justification': "That's not what your mother said last night Trebek"
-}, {
-  'name': 'Teenage Mutant Ninja',
-  'justification': 'Turdles'
-}, {
-  'name': 'Donald Dump',
-  'justification': "I POOP, I have da best poops, they're UUGE"
-}]
 app.listen(3000)
 
 /* app.get('/',function(req,res){
